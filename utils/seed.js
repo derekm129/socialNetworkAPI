ebc72bb9340c214b9112e11bf04a60be79481b33
dmod129 <derek.modugno@gmail.com>
2023-12-17 11:58:42 -0600

    created seed.js

---
+++ utils/seed.js
@@ -0,0 +1,25 @@
+const connection = require('../config/connection');
+const { User, Thought, Reaction } = require('../models');
+
+const users = [
+    {
+        username: 'Allen',
+        email: 'allen@gmail.com',
+    },
+    {
+        username: 'Billy',
+        email: 'billy@gmail.com',
+    },
+    {
+        username: 'Caroline',
+        email: 'caroline@gmail.com',
+    },
+    {
+        username: 'Dennis',
+        email: 'dennis@gmail.com',
+    },
+    {
+        username: 'Emily',
+        email: 'emily@gmail.com',
+    },
+]
